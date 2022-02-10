aa0341b909eee95a9317a6dadbb79c903b9394c0
NiiFrance <45998131+NiiFrance@users.noreply.github.com>
2022-02-10 23:32:22 +0000

    Update setup.sh
    Updated Setup.sh script

--- setup.sh
+++ setup.sh
@@ -23,4 +23,7 @@ sudo useradd -m -d $user_dir $sys_user
 
 echo "Cloning forked repository into home directory of user..."
 cd $user_dir
+
+echo "removing existing files from user directory..."
+sudo rm -rf $user_dir/*
 sudo git clone https://github.com/NiiFrance/Mobalysis.git
