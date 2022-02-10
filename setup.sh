edadd94f57f6a5bbb9d99468c526443eecb4e9bd
NiiFrance <45998131+NiiFrance@users.noreply.github.com>
2022-02-10 23:04:00 +0000

    Update setup.sh
    Added creation of user with home directory

--- setup.sh
+++ setup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-
+echo "updating and checking for package updates..."
 sudo apt-get update -y
 db_user="mob_db_user"
 db_pwd="mob_db_pass"
+sys_user="mob_app_usr"
 packages=('git' 'gcc' 'tar' 'gzip' 'libreadline5' 'make' 'zlib1g' 'zlib1g-dev' 'flex' 'bison' 'perl' 'python3' 'tcl' 'gettext' 'odbc-postgresql' 'libreadline6-dev')
+
 echo "Installing PostgreSQL dependencies"
 sudo apt-get install ${packages[@]} -y
+
+echo "installing postgres..."
 sudo apt-get install postgresql -y
 sudo /etc/init.d/postgresql start
 
+echo "adding user to postgres with password..."
 sudo -u postgres createuser -s -i -d -r -l -w $db_user
 sudo -u postgres psql -c "ALTER ROLE $db_user WITH PASSWORD '$db_pwd' ";
+
+echo "creating user with home directory..."
+sudo useradd -m -d /home/$sys_user $sys_user
